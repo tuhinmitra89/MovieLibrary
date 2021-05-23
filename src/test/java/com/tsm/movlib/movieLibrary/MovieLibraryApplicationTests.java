9d44841d63fc8abcc75ffe44084661a4b57c6620
tuhinmitra89 <tuhinmitra89@gmail.com>
2021-05-24 01:03:00 +0530

    Initial Checkin

---
+++ src/test/java/com/tsm/movlib/movieLibrary/MovieLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsm.movlib.movieLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
